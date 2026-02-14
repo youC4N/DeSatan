e3c5c47878b4d145530fe623d197ab994616d68b
Heorhii Malyhin <hmalyhin@griddynamics.com>
2026-02-14 21:34:07 +0100

    Initial Commit

---
+++ DeSatan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeSatan
+//
+//  Created by Heorhii Malyhin on 14.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeSatan/DeSatanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeSatanApp.swift
+//  DeSatan
+//
+//  Created by Heorhii Malyhin on 14.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct DeSatanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
